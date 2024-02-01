6d5293392692da4abcfae82acf6f960eb4292703
Anupam Kumar <54003614+Anupamsingh12@users.noreply.github.com>
2024-02-01 23:02:57 +0530

    initial commit

---
+++ src/pages/Inventory.js
@@ -0,0 +1,211 @@
+import React, { useState, useEffect, useContext } from "react";
+import AddProduct from "../components/AddProduct";
+import UpdateProduct from "../components/UpdateProduct";
+import AuthContext from "../AuthContext";
+
+const DummyData = [
+  {
+    _id: '1',
+    name: 'Product 1',
+    manufacturer: 'Manufacturer A',
+    stock: 10,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    _id: '2',
+    name: 'Product 2',
+    manufacturer: 'Manufacturer B',
+    stock: 0,
+    description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  // Add more dummy data as needed
+];
+
+function Inventory() {
+  const [showProductModal, setShowProductModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [updateProduct, setUpdateProduct] = useState([]);
+  const [products, setAllProducts] = useState([]);
+  const [data,setData] = useState([])
+  const [searchTerm, setSearchTerm] = useState();
+  const [updatePage, setUpdatePage] = useState(true);
+  const [stores, setAllStores] = useState([]);
+
+  const authContext = useContext(AuthContext);
+
+  useEffect( () => {
+     fetchProductsData();
+   
+  }, []);
+
+  // Fetching Data of All Products
+  const fetchProductsData =  () => {
+    // setAllProducts(DummyData)
+    fetch(`https://hotel-backend-ahus.onrender.com/api/rooms`)
+      .then((response) => response.json())
+      .then((data) => {
+        setAllProducts(data.rooms);
+      })
+      .catch((err) => console.log(err));
+  };
+
+ 
+
+
+
+  // Modal for Product ADD
+  const addProductModalSetting = () => {
+    setShowProductModal(!showProductModal);
+  };
+
+  // Modal for Product UPDATE
+  const updateProductModalSetting = (selectedProductData) => {
+    console.log("Clicked: edit");
+    setUpdateProduct(selectedProductData);
+    setShowUpdateModal(!showUpdateModal);
+  };
+
+
+  // Delete item
+  const deleteItem = (id) => {
+    console.log("Product ID: ", id);
+    console.log(`http://localhost:4000/api/product/delete/${id}`);
+    fetch(`http://localhost:4000/api/product/delete/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUpdatePage(!updatePage);
+      });
+  };
+
+  // Handle Page Update
+  const handlePageUpdate = () => {
+    setUpdatePage(!updatePage);
+  };
+
+  // Handle Search Term
+  const handleSearchTerm = (e) => {
+    setSearchTerm(e.target.value);
+    // fetchSearchData();
+  };
+
+  return (
+    <div className="col-span-12 lg:col-span-10  flex justify-center">
+      <div className=" flex flex-col gap-5 w-11/12">
+        <div className="bg-white rounded p-0">
+
+          {/* <span className="font-semibold px-4">Overall Inventory</span> */}
+           <div className="flex justify-between pt-5 pb-3 px-2">
+            <div className="flex gap-4 justify-center items-center ">
+              <span className="font-bold">Rooms</span>
+              <div className="flex justify-center items-center px-2 border-2 rounded-md ">
+                <img
+                  alt="search-icon"
+                  className="w-5 h-5"
+                  src={require("../assets/search-icon.png")}
+                />
+                <input
+                  className="border-none outline-none focus:border-none text-xs"
+                  type="text"
+                  placeholder="Search here"
+                  value={searchTerm}
+                  onChange={handleSearchTerm}
+                />
+              </div>
+            </div>
+            <div className="flex gap-4">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 text-xs  rounded"
+                onClick={addProductModalSetting}
+              >
+                Add Room
+              </button>
+            </div>
+          </div>
+          
+        </div>
+
+        {showProductModal && (
+          <AddProduct
+            addProductModalSetting={addProductModalSetting}
+            handlePageUpdate={handlePageUpdate}
+          />
+        )}
+        {showUpdateModal && (
+          <UpdateProduct
+            updateProductData={updateProduct}
+            updateModalSetting={updateProductModalSetting}
+          />
+        )}
+
+        {/* Table  */}
+        <div className="overflow-x-auto rounded-lg border bg-white border-gray-200 ">
+         
+          <table className="min-w-full divide-y-2 divide-gray-200 text-sm">
+            <thead>
+              <tr>
+                <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 font-bold">
+                  Room Number
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 font-bold">
+                  Type
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 font-bold">
+                  Price
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 font-bold">
+                  Status
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 font-bold">
+                  Available
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 font-bold">
+                  More
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="divide-y divide-gray-200">
+              {products.map((element, index) => {
+                return (
+                  <tr key={element._id}>
+                    <td className="whitespace-nowrap px-4 py-2  text-gray-900">
+                      {element.roomNumber}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      {element.type}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      {element.price}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      {element.status}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      {10}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      <span
+                        className="text-green-700 cursor-pointer"
+                        onClick={() => updateProductModalSetting(element)}
+                      >
+                        Edit{" "}
+                      </span>
+                      <span
+                        className="text-red-600 px-2 cursor-pointer"
+                        onClick={() => deleteItem(element._id)}
+                      >
+                        Delete
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Inventory;
